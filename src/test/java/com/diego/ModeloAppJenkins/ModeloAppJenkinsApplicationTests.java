c84a692c9b036509850e8905d2fdd01b2d2a588a
diego-mesiano <diego.mesiano1@gmail.com>
2021-11-23 14:34:36 -0300

    Subindo projeto maven

---
+++ src/test/java/com/diego/ModeloAppJenkins/ModeloAppJenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diego.ModeloAppJenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModeloAppJenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
